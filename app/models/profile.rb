cdeb6aeecd4be1e1529f42bb24e80f51e51270d9
munetomo <fishmans0120@gmail.com>
2020-10-17 13:04:50 +0900

    タグ機能実装
    - 複数タグ付け（カンマ区切り）
    - タグ作成、編集、削除ができる
    - 削除の場合はリレーションが消える

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,49 +1,72 @@
 class Profile < ApplicationRecord
- has_many :tags, through: :profile_tags
- has_many :genres, through: :genre_profiles
- has_many :content
- belongs_to :user
+  # 先に中間テーブルとのアソシエーションを書くこと
+  has_many :profile_tags, dependent: :destroy
+  has_many :tags, through: :profile_tags, dependent: :destroy
+  has_many :genres, through: :genre_profiles
+  has_many :content
+  belongs_to :user
 #  アクティブハッシュ（都道府県）
- extend ActiveHash::Associations::ActiveRecordExtensions
- belongs_to_active_hash :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
 #  バリデーション
-validates :first_name, presence: true, length: {maximum: 10}
-validates :family_name, presence: true, length: {maximum: 10}
-validates :first_name_kana, presence: true, length: {maximum: 20}
-validates :family_name_kana, presence: true, length: {maximum: 20}
-validates :city, presence: true, length: {maximum: 20}
-validates :post_code, presence: true, length: {maximum: 7}
-validates :address_number, presence: true, length: {maximum: 40}
-validates :building, length: {maximum: 40}
-validates :primary_school, length: {maximum: 20}
-validates :Junior_high_school, length: {maximum: 20}
-validates :high_school, length: {maximum: 20}
-validates :vocational_school, length: {maximum: 40}
-validates :university, length: {maximum: 40}
-validates :graduate_school, length: {maximum: 40}
-validates :other_school, length: {maximum: 40}
-validates :first_career, length: {maximum: 40}
-validates :second_career, length: {maximum: 40}
-validates :third_career, length: {maximum: 40}
-validates :fourth_career, length: {maximum: 40}
-validates :last_career, length: {maximum: 40}
-validates :introduction, length: {maximum: 500}
-validates :birth_date, presence: true
-validates :phone, length: {maximum: 11}
-validates :hp, length: {maximum: 60}
-validates :facebook, length: {maximum: 60}
-validates :twitter, length: {maximum: 60}
-validates :catch_copy, length: {maximum: 60}
-validates :avatar_title, length: {maximum: 40}
-validates :avatar_about, length: {maximum: 500}
+  validates :first_name, presence: true, length: {maximum: 10}
+  validates :family_name, presence: true, length: {maximum: 10}
+  validates :first_name_kana, presence: true, length: {maximum: 20}
+  validates :family_name_kana, presence: true, length: {maximum: 20}
+  validates :city, presence: true, length: {maximum: 20}
+  validates :post_code, presence: true, length: {maximum: 7}
+  validates :address_number, presence: true, length: {maximum: 40}
+  validates :building, length: {maximum: 40}
+  validates :primary_school, length: {maximum: 20}
+  validates :Junior_high_school, length: {maximum: 20}
+  validates :high_school, length: {maximum: 20}
+  validates :vocational_school, length: {maximum: 40}
+  validates :university, length: {maximum: 40}
+  validates :graduate_school, length: {maximum: 40}
+  validates :other_school, length: {maximum: 40}
+  validates :first_career, length: {maximum: 40}
+  validates :second_career, length: {maximum: 40}
+  validates :third_career, length: {maximum: 40}
+  validates :fourth_career, length: {maximum: 40}
+  validates :last_career, length: {maximum: 40}
+  validates :introduction, length: {maximum: 500}
+  validates :birth_date, presence: true
+  validates :phone, length: {maximum: 11}
+  validates :hp, length: {maximum: 60}
+  validates :facebook, length: {maximum: 60}
+  validates :twitter, length: {maximum: 60}
+  validates :catch_copy, length: {maximum: 60}
+  validates :avatar_title, length: {maximum: 40}
+  validates :avatar_about, length: {maximum: 500}
 
-# image_uploader
-# アバター写真
-mount_uploader :avatar, ImageUploader
-# メイン写真
-mount_uploader :image, ImageUploader
-# メイン写真
-mount_uploader :sub_image, ImageUploader
+  # image_uploader
+  # アバター写真
+  mount_uploader :avatar, ImageUploader
+  # メイン写真
+  mount_uploader :image, ImageUploader
+  # メイン写真
+  mount_uploader :sub_image, ImageUploader
+
+  def save_tags(saveprofile_tags)
+    # saveprofile_tags.each do |new_name|
+    #   profile_tag = Tag.find_or_create_by(tag_name: new_name)
+    #   self.tags << profile_tag
+    # end
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - saveprofile_tags
+    new_tags = saveprofile_tags - current_tags
+
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(tag_name: old_name)
+    end
+
+    new_tags.each do |new_name|
+      profile_tag = Tag.find_or_create_by(tag_name: new_name)
+      self.tags << profile_tag
+    end
+    
+  end
+
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :profile, through: :profile_tags
+  # 先に中間テーブルとのアソシエーションを書くこと
+  has_many :profile_tags, dependent: :destroy
+  has_many :profiles, through: :profile_tags, dependent: :destroy
 end
