0623f45ec0ebcde3f2d9c504e667d3243ddee147
yuktnk <yuki0wl@gmail.com>
2020-11-05 17:27:15 +0900

    編集機能途中（心が折れそうです）

--- app/assets/javascripts/photogallery.js
+++ app/assets/javascripts/photogallery.js
@@ -21,13 +21,31 @@ $(function(){
                   `;
     return html;
   }
+  const buildFileField2 = ()=> {
+    const html = `<div class="preview">
+                    <button type="button" class="btn btn-outline-secondary test" >
+                      画像を追加
+                    </button>
+                  </div>
+                  `;
+    return html;
+  }
 
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
 
-  // 選択された画像が5枚未満の場合に、
+  let PreviewLength = $('.preview').length
+  console.log(PreviewLength);
+  if (PreviewLength < 10) {
+    $('#image-previews').append(buildFileField2(fileIndex[0]));
+
+    $('#image-box').append(buildFileField(fileIndex[PreviewLength]));
+  }
+
+
+  // 選択された画像が10枚未満の場合に、
   // fileIndexの先頭の数字を使って画像用のinputを生成する
   $('#image-box').on('change', '.js-file', function(e) {
     $(document).ready(function(){
@@ -52,7 +70,51 @@ $(function(){
       $('#image-box').append(buildFileField(fileIndex[0]));
     }
   });
+
+
+  // 削除ボタン
+  $('#image-previews').on('click', '.delete-img', function() {
+    const targetIndex = $(this).parent().parent().parent().data('index');
+
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"]`);
+
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+
+    $(this).parent().parent().parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
+    // $('.js-file_group').remove();
+
+
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.preview').length === 0) {
+      console.log("ないよ");
+      $('#image-box').append(buildFileField(fileIndex[0]))
+    };
+  });
+
+  // 変更ボタン
+  $('#image-previews').on('click', '.change-img', function() {
+    const targetIndex = $(this).parent().parent().parent().data('index');
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenChange = $(`input[id="profile_images_attributes_${targetIndex}_src"]`);
+    console.log(hiddenChange);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenChange) hiddenChange.click();
+
+    // $(this).parent().parent().remove();
+    // $(`img[data-index="${targetIndex}"]`).remove();
+
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file').length == 0) {
+      $('#image-box').append(buildFileField(fileIndex[0]))
+    };
+  });
 });
+
+
+
 
 
 // モーダル表示用プラグイン「Modaal」のための記述
