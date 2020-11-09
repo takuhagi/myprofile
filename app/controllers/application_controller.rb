2a0df62156eeedd525a7ed29100364c1ecfcc3f8
takuhagi <h.takumi52@gmail.com>
2020-11-10 00:24:43 +0900

    サインアップビューの改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,17 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:name])
   end
+
 end
 
+
+#   protect_from_forgery except: :search # searchアクションを除外
+#     def index
+#     end
+
+#     def search
+#       @products = Product.where('name LIKE(?)', "%#{params[:keyword]}%")
+#       redirect_to 'index'
+#     end
+#   end  
+# end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  
+ 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -58,8 +59,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.raise_delivery_errors = true
