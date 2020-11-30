b2140762b0694f3c3a33c2a13e4a5e528e1c9789
takuhagi <h.takumi52@gmail.com>
2020-11-30 15:02:49 +0900

    ログイン機能でuser_nameカラムを使用しなくなったため記述を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
       port:                 587,
       address:              'smtp.gmail.com',
       domain:               'gmail.com',
-      user_name:             Rails.application.credentials[:gmail][:user_name],
+      # user_name:             Rails.application.credentials[:gmail][:user_name],
       password:              Rails.application.credentials[:gmail][:password],
       authentication:       'login',
       enable_starttls_auto: true
