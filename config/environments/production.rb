dc45b5129ac83644c4bec96ae203645381acbd6a
takuhagi <h.takumi52@gmail.com>
2020-12-06 18:30:35 +0900

    本番環境メール認証機能実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,18 +110,18 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
      
-     config.action_mailer.default_url_options = { host: 'https://3.114.61.191' }
-     config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-     config.action_mailer.raise_delivery_errors = true
-     config.action_mailer.delivery_method = :smtp
-     config.action_mailer.smtp_settings = {
-      port:                 587,
-      address:              'smtp.gmail.com',
-      domain:               'gmail.com',
-      user_name:             Rails.application.credentials[:gmail][:user_name],
-      password:              Rails.application.credentials[:gmail][:password],
-      authentication:       'login',
-      enable_starttls_auto: true
-    }  
+  #    config.action_mailer.default_url_options = { host: 'https://3.114.61.191' }
+  #    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #    config.action_mailer.raise_delivery_errors = true
+  #    config.action_mailer.delivery_method = :smtp
+  #    config.action_mailer.smtp_settings = {
+  #     port:                 587,
+  #     address:              'smtp.gmail.com',
+  #     domain:               'gmail.com',
+  #     user_name:             Rails.application.credentials[:gmail][:user_name],
+  #     password:              Rails.application.credentials[:gmail][:password],
+  #     authentication:       'login',
+  #     enable_starttls_auto: true
+  #   }  
    
-   end
+  #  end
