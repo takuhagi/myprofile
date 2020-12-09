61a68c281cf4f7b1568848a55add7f7f0c6d0855
takuhagi <h.takumi52@gmail.com>
2020-12-09 21:21:24 +0900

    環境変数実験（メール認証機能実装のため）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:             Rails.application.credentials[:gmail][:user_name],
-    password:              Rails.application.credentials[:gmail][:password],
+    user_name:             ENV["BASIC_AUTH_USER"],
+    password:              ENV["BASIC_AUTH_PASSWORD"],
     authentication:       'login',
     enable_starttls_auto: true
   }  
