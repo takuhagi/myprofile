588002cc1b13d91d914021ac9d4ea5cca13aa7a2
takuhagi <h.takumi52@gmail.com>
2020-12-12 16:44:23 +0900

    S3画像保存のための修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,6 +20,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
 
-  config.fog_directory  = 'myprofile-gazou'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myprofile-gazou'
+    config.fog_directory  = 'myprofile-gazou'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myprofile-gazou'
+  end
 end
