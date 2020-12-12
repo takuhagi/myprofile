dd6aff20fa2de6b799e2100f5d491e37856e41ba
takuhagi <h.takumi52@gmail.com>
2020-12-12 17:11:13 +0900

    before_action解除
    トップ画面をログインなしで誰でも見られるようにするため before_actionを解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:rule, :privacy] #ログインしてなかったらログインへ 利用規約とプライバシーポリシーは外す
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!, except: [:rule, :privacy] #ログインしてなかったらログインへ 利用規約とプライバシーポリシーは外す
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
