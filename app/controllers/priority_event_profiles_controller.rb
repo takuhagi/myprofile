9728797607157ffa04d377b57862ce8fa919acdc
ShuheiOhno <inu.suika8@gmail.com>
2020-12-20 17:41:37 +0900

    管理画面リダイレクト（仮）

--- app/controllers/priority_event_profiles_controller.rb
+++ app/controllers/priority_event_profiles_controller.rb
@@ -1,10 +1,15 @@
 class PriorityEventProfilesController < ApplicationController
   def create
     PriorityEventProfile.create(priority_event_profile_params)
+    redirect_to event_profiles_path
+    # redirect_back fallback_location: priority_store_profiles_path
+    # redirect_to action: 'index'
   end
 
   def destroy
     PriorityEventProfile.find(params[:id]).destroy
+    # redirect_back fallback_location: priority_store_profiles_path
+    redirect_to event_profiles_path
   end
 
   def priority_event_profile_params
