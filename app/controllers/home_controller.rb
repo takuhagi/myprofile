d584b5d12e72d75abb55fbea813dd29e504680d8
masa-ki11 <67939220+masa-ki11@users.noreply.github.com>
2020-12-20 19:34:18 +0900

    修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+
+  require "payjp"
+  before_action :pay, only: [:index]
+
   def index
     @profiles = Profile.all.includes([:user])
     @profilet = Profile.where(top: [1]).includes([:user])
@@ -8,4 +12,24 @@ class HomeController < ApplicationController
     @rank_profiles = @profiles.order(pv_count: "DESC").limit(5)
     @new_profiles = @profiles.order(created_at: "DESC").limit(5)
   end
+
+  private
+
+  def pay
+    Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
+    @card = Card.all
+    subscription = Payjp::Subscription.all
+    subscription.each do |s|
+      if s.status == "paused"
+        customer = Payjp::Customer.retrieve(s.customer)
+        customer.delete
+        @card.each do |card|
+          if card.customer_id == customer.id
+            card.delete
+          end
+        end
+      end
+    end
+  end
+  
 end
